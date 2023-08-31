2d40bfaca79b0f656a90d1ad4c36a822ab6b32d9
anupbhadraa <anupbhadra1105@gmail.com>
2023-08-31 23:58:27 +0530

    Exercise3

---
+++ cwh_43_exrecise3.java
@@ -0,0 +1,13 @@
+public class cwh_43_exrecise3 {
+    public static void main(String[] args) {
+        /*
+         create a class game, which allows a user to play "Guess the Number" game once. Gmae should have the
+         following methods.
+         1. constructor for generate the random number
+         2. takeuserInput() to take a user input of number
+         3. isCorrectnumber() to detect whether the number entered by the user is true
+         4. getter & setter fo noOfguesses
+         
+         */
+    }
+}
