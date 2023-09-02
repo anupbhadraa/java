36e153f3689f78e13df1bd0ece3d758128560fc1
anupbhadraa <anupbhadra1105@gmail.com>
2023-09-02 23:57:41 +0530

    minor change

--- cwh_46_constructor_in_inheritance.java
+++ cwh_46_constructor_in_inheritance.java
@@ -8,6 +8,7 @@ class Base1 {
     public Base1(int x) {
         System.out.println("i am an overloaded constructor with a value of a as: "+x); 
     }
+    
 
     public void setx(int x) {
         this.x = x;
@@ -41,12 +42,24 @@ class Derived1 extends Base1 {
     }
 }
 
+class childOFDerived extends Derived1 {
+    childOFDerived() {
+        System.out.println("I am a child of derived constructor");
+    }
+
+    childOFDerived(int x, int y, int z) {
+        super(x, y);
+        System.out.println("I am an overloaded constructor of Derived with value of z as: " + z);
+    }
+}
+
 
 public class cwh_46_constructor_in_inheritance {
     public static void main(String [] args) {
         // Base1 b = new Base1();
         // Derived1 d = new Derived1();
-        Derived1 d = new Derived1(4, 5); 
+        // Derived1 d = new Derived1(4, 5); 
+        childOFDerived child = new childOFDerived(10, 20, 30);
     }
     
 }
