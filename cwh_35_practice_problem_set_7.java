f6b301aa94b2d4cc12f431f33315025a7a07e98c
anupbhadraa <anupbhadra1105@gmail.com>
2023-09-06 23:57:43 +0530

    minor change

--- cwh_35_practice_problem_set_7.java
+++ cwh_35_practice_problem_set_7.java
@@ -1,27 +1,9 @@
 public class cwh_35_practice_problem_set_7 {
 
-    //problem 6
-    static void avg(int [] avg) {
-        int sum = 0;
-        int average = 0;
-
-        for( int element: avg ) {
-            sum = sum + element;
-
-            average = sum / avg.length;
-
-        }
-        System.out.println("Average: " + average);
-
-    }
-
-    public static void main(String[] args) {
-        
-        
-        /* 
-        
-        //Problem 1
-        static void multi_table(int n) {
+    /* 
+     
+    //Problem 1
+    static void multi_table(int n) {
         for(int i = 1; i <= 10; i++) {
             int result = n*i;
             System.out.println(n + "*" + i + " = " + result);
@@ -45,9 +27,9 @@ public class cwh_35_practice_problem_set_7 {
     }
     
     */
-    
+
     /*
-    
+     
     //Problem 3
     static void naturalNoSum(int n) {
         int sum = 0;
@@ -61,9 +43,6 @@ public class cwh_35_practice_problem_set_7 {
     
     */
 
-    /* 
-     
-    
     //Problem 4
     static void statPattern2(int n) {
         for(int i=1; i<=n; i++ ) {
@@ -79,43 +58,6 @@ public class cwh_35_practice_problem_set_7 {
         // starPattern(4);
         // naturalNoSum(5);
         statPattern2(4);
-        
+
     }
-    
-    */
-    
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /* 
-     
-    //problem 5
-    int a = 0, b = 1, c;
-    int n = 10;
-    
-    System.out.print(a + " ");
-    for(int i=1; i<=n ; i++) {
-        c = a + b;
-        System.out.print(c + " ");
-        a = b;
-        b = c;
-    }
-
-    */
-
-    ////////////////////////////////////////////////////////////////
-     
-    /*
-     
-    //problem 6
-    int [] arr = {23, 45, 67, 89, 87};
-    avg(arr);
-    
-    */
-    
-    
-
-    
-}
-    
-
 }
