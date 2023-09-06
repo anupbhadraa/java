d7bb9eec8eae6cc81acc98f93537a15aaa2211f6
anupbhadraa <anupbhadra1105@gmail.com>
2023-09-06 23:55:34 +0530

    array basics

---
+++ ArrCC.java
@@ -0,0 +1,10 @@
+public class ArrCC {
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]+ " ");
+        }
+    }
+    
+}
